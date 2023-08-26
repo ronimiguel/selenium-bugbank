6aed1f69d8b111c809f48948fd0e051f7d6d2e7d
Roni Miguel <ronimiguel@gmail.com>
2023-08-26 19:39:48 -0300

    feat: Adicionado HomePage com metodos de interação da pagina

---
+++ src/main/java/com/bugbank/pages/HomePage.java
@@ -0,0 +1,28 @@
+package com.bugbank.pages;
+
+import com.bugbank.utils.Cliente;
+import com.bugbank.utils.DadosContaUtils;
+import com.bugbank.utils.WebElementActions;
+import org.openqa.selenium.By;
+import static com.bugbank.pages.Locators.*;
+
+public class HomePage extends WebElementActions {
+    DadosContaUtils dadosContaUtils = new DadosContaUtils();
+
+    public void clicarBotaoSair(){
+        clicar(SAIR_BTN);
+    }
+
+    public void clicarBotaoTransferencia(){
+        clicar(TRANSFERENCIA_BTN);
+    }
+
+    public void atualizarSaldoInicialCliente(Cliente cliente){
+        dadosContaUtils.atualizarSaldoInicial(cliente);
+    }
+
+    public void atualizarSaldoAtualCliente(Cliente cliente){
+        dadosContaUtils.atualizarSaldoAtual(cliente);
+    }
+
+}
