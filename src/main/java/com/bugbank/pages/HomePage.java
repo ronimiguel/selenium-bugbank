cc5e03cd114bf2db7982eec8026620fcfb12395f
Roni Miguel <ronimiguel@gmail.com>
2023-08-27 19:16:10 -0300

    remove: Limpeza de codigo

--- src/main/java/com/bugbank/pages/HomePage.java
+++ src/main/java/com/bugbank/pages/HomePage.java
@@ -3,7 +3,7 @@ package com.bugbank.pages;
 import com.bugbank.utils.Cliente;
 import com.bugbank.utils.DadosContaUtils;
 import com.bugbank.utils.WebElementActions;
-import org.openqa.selenium.By;
+
 import static com.bugbank.pages.Locators.*;
 
 public class HomePage extends WebElementActions {
